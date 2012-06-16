1c4783159838991ae477f0076fb203223f00e47c
tamerdark <tamerdark@gmail.com>
2012-06-16 16:59:59 -0400

    Implemented two different classes of NieghborSelector

---
+++ src/cvrp/classes/BestNeighborSelector.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cvrp.classes;
+
+import cvrp.interfaces.NeighborSelector;
+import java.util.List;
+
+/**
+ *
+ * @author tamerdark
+ */
+public class BestNeighborSelector implements NeighborSelector {
+
+    @Override
+    public Neighbor selectNeighbor(List<Neighbor> neighbors, Solution bestFound) {
+        Neighbor best = neighbors.get(0);
+        for (Neighbor n: neighbors){
+            if( n.getDuration() < best.getDuration() ){
+                best = n;
+            }
+        }
+        return best;
+    }
+    
+}
