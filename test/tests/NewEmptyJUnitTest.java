009eef986cc1171141f8996ca8eec37a3aa7a8f5
Vicente Santacoloma <vsantacoloma@gmail.com>
2012-06-17 11:41:48 -0430

    Se arreglaron conflictos

---
+++ test/tests/NewEmptyJUnitTest.java
@@ -0,0 +1,49 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author vicente
+ */
+public class NewEmptyJUnitTest {
+  
+  public NewEmptyJUnitTest() {
+  }
+
+  @org.junit.BeforeClass
+  public static void setUpClass() throws Exception {
+  }
+
+  @org.junit.AfterClass
+  public static void tearDownClass() throws Exception {
+  }
+
+  @org.junit.Before
+  public void setUp() throws Exception {
+  }
+
+  @org.junit.After
+  public void tearDown() throws Exception {
+  }
+  
+  @Before
+  public void setUp() {
+  }
+  
+  @After
+  public void tearDown() {
+  }
+  // TODO add test methods here.
+  // The methods must be annotated with annotation @Test. For example:
+  //
+  // @Test
+  // public void hello() {}
+}
